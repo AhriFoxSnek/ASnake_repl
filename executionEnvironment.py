ccd29281b07fe58d0f2d9ae806c186852af4f848
Ahri Fox <89772420+AhriFoxSnek@users.noreply.github.com>
2022-05-11 14:37:51 -0700

    remove comments

--- executionEnvironment.py
+++ executionEnvironment.py
@@ -9,8 +9,6 @@ while True:
         except Exception as e:
             print(chr(999998))
             print('Python ' + e.__class__.__name__ + ':\n' + str(e))
-            #stdscr.addstr(error, curses.color_pair(3))
-            #stdscr.move(y + error.count('\n') + 2, 0)
         remove('ASnakeREPLCommand.txt')
         print(chr(999999))
     else:
