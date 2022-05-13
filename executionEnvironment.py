9fbec76b75ee58bd74e7663ca91619ff2adc7b9a
Ahri Fox <89772420+AhriFoxSnek@users.noreply.github.com>
2022-05-12 23:45:03 -0700

    execGlobal not needed, no newline on errorByte

--- executionEnvironment.py
+++ executionEnvironment.py
@@ -1,13 +1,12 @@
-execGlobal = globals()
 from os import listdir, remove
 from time import sleep
 while True:
     if 'ASnakeREPLCommand.txt' in listdir():
         try:
             with open('ASnakeREPLCommand.txt', 'r') as _:
-                exec(_.read(), execGlobal)
+                exec(_.read())
         except Exception as e:
-            print(chr(999998))
+            print(chr(999998),end='')
             print('Python ' + e.__class__.__name__ + ':\n' + str(e))
         remove('ASnakeREPLCommand.txt')
         print(chr(999999))
