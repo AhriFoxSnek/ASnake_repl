771a3828dbe589526e854775bc19286c643e2244
Ahri Fox <89772420+AhriFoxSnek@users.noreply.github.com>
2022-05-14 06:37:16 -0700

    handles KeyboardInterrupt to stop execution.
    i dont know why it needs two separate exception handlers but that is the only way it seems to be consistent in not outputting an error. also exits on KeyboardInterrupt on sleep because no commands are running, and thus can assume we are exiting.

--- executionEnvironment.py
+++ executionEnvironment.py
@@ -4,11 +4,23 @@ while True:
     if 'ASnakeREPLCommand.txt' in listdir():
         try:
             with open('ASnakeREPLCommand.txt', 'r') as _:
-                exec(_.read())
+                try:
+                    exec(_.read())
+                except KeyboardInterrupt:
+                    try:
+                        remove('ASnakeREPLCommand.txt')
+                    except: raise KeyboardInterrupt
+        except KeyboardInterrupt:
+            pass
         except Exception as e:
             print(chr(999998),end='')
             print('Python ' + e.__class__.__name__ + ':\n' + str(e))
-        remove('ASnakeREPLCommand.txt')
-        print(chr(999999))
+        finally:
+            try: remove('ASnakeREPLCommand.txt')
+            except: pass
+            print(chr(999999))
     else:
-        sleep(0.01)
+        try:
+            sleep(0.01)
+        except KeyboardInterrupt:
+            exit()
