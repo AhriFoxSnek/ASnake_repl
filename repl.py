b1bc9b3bf6d2d3e85609febf7ab04d2bde34e56d
Ahri Fox <89772420+AhriFoxSnek@users.noreply.github.com>
2024-10-18 15:20:48 -0700

    syntax highlighting, indent blocks, auto indent, tons of fixes, more bugs added. its a grand ol time

--- repl.py
+++ repl.py
@@ -65,7 +65,7 @@ else:
 del sys, compileDict, platform
 
 # constants
-ReplVersion = 'v0.10.0'
+ReplVersion = 'v0.11.0'
 PREFIX = ">>> "
 PREFIXlen = len(PREFIX)
 INDENT = "... "
@@ -73,7 +73,7 @@ INDENTlen = len(INDENT)
 SPACE = '  '
 SPACElen = len(SPACE)
 checkIfIndent   = re_compile('^(?: |\t)*(?:if|elif|case|of|else|while|def) .+(:|;| do| then) *$').search
-exitIndent      = re_compile(f'\n{SPACE}+$').search
+exitIndent      = re_compile(f'\n+(?:{SPACE})*$').search
 deIndent        = re_compile(f'\n{SPACE}+').findall
 
 
@@ -81,8 +81,6 @@ deIndent        = re_compile(f'\n{SPACE}+').findall
 def file_out(write_mode, *args):
     with open("streams.txt", write_mode, encoding='utf-8') as f:
         f.write(f"{args}\n")
-
-
 
 trimSuggestion = re_compile(f"[{re_escape(' ()*+-/,=&^%$#@')}]+")
 def get_last_substring(s, return_last=True, return_index=False):
@@ -151,7 +149,7 @@ def buildCode(code, variableInformation, metaInformation):
 
 
 bash_history = []
-stopCharacters = set(" .()*+-/,'\"=&^%$#@")
+stopCharacters = set(" .()*+-/,'\"=&^%$#@{}")
 keyword_list = ('__build_class__', '__debug__', '__doc__', '__import__', '__loader__', '__name__', '__package__',
                 '__spec__', 'abs', 'all', 'and', 'any', 'ArithmeticError', 'as', 'ascii', 'assert', 'AssertionError',
                 'async', 'AttributeError', 'await', 'BaseException', 'bin', 'BlockingIOError', 'bool', 'break',
@@ -213,7 +211,7 @@ def main(stdscr):
     KEY_TAB = ord('\t')
     KEY_BACKSPACE = {curses.KEY_BACKSPACE, 127, 8, 330}
     KEY_ENTER = {curses.KEY_ENTER, 10, 13}
-    CTRL_LEFT = {ord('Ȧ'),ord('Ȫ'),ord('ȫ'), 564}
+    CTRL_LEFT = {ord('Ȧ'),ord('Ȫ'),ord('ȫ'), 564, 546}
     CTRL_RIGHT = {ord('ȵ'), ord('ȹ'), ord('ŋ'), 561}
     CTRL_W = ord(b'\x17')
     # shift left 393 ; shift right 402
@@ -225,15 +223,24 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_WHITE, -1)  # for usual text
     curses.init_pair(2, curses.COLOR_CYAN, -1)  # for pretty prefix
     curses.init_pair(3, curses.COLOR_RED, -1) # for scary error
+    curses.init_pair(5, curses.COLOR_GREEN, -1) # for strings
+    curses.init_pair(6, curses.COLOR_MAGENTA, -1) # for numbers
     if isWindows:
         curses.init_pair(4, curses.COLOR_YELLOW, -1)
         stdscr.nodelay(1)
     curses.echo()
 
-    # v debug vars v
-    debug: bool = False  # enables file output of useful info for debugging
+    # vv debug vars vv
+    debug: bool = True  # enables file output of useful info for debugging
     extra = ''
     debugFileOut=False
+    debugTryCommands = []
+    #debugTryCommands = ['if 1 do',list(KEY_ENTER)[1],list(KEY_ENTER)[1],'12',list(KEY_ENTER)[1],curses.KEY_UP,curses.KEY_UP,chr(575),'2',chr(534),'3']
+    #debugTryCommands = ['if True do', list(KEY_ENTER)[1], '12', list(KEY_ENTER)[1], list(KEY_ENTER)[1], curses.KEY_UP, curses.KEY_UP, list(KEY_ENTER)[1],curses.KEY_UP, curses.KEY_DOWN]
+    # ^ does command on startup to test a specific case
+    debugTryCommands = [ord(char) if isinstance(item, str) else item for item in debugTryCommands for char in (str(item) if isinstance(item, str) else [item])]
+    # ^ auto converts for understandable format for the REPL
+    # -- debug var end --
 
     # v normal vars v
     after_appending = 0
@@ -242,6 +249,7 @@ def main(stdscr):
     firstLine: bool = True
     inIndent: bool = False
     currentIndent: int = 0
+    inString = False # only values: False 34 39
 
     code = ''
     variableInformation = {}
@@ -301,9 +309,6 @@ def main(stdscr):
             else:  tmpY += prefaceLEN
         if delete:
             delete_line(stdscr=stdscr, start=x, end=tmpPosition, step=-1, y=y)
-        #extra = f'{direction} {width=} {codeLength=} tmpPosition={tmpPosition-1 if direction == 'right' else tmpPosition+1} | {tmpY=}'
-        #with open('test.txt','w') as f:
-        #    f.write(extra)
         stdscr.move(tmpY, tmpPosition-1 if direction == 'right' else tmpPosition+1)
 
     def exitRoutine():
@@ -320,31 +325,112 @@ def main(stdscr):
         exit()
 
     def redraw(stdscr):
+        nonlocal x,y
         delete_line(stdscr=stdscr, start=PREFIXlen, end=width, step=1, y=linesStartingY)
         for yy in range(y + 1, height):
             delete_line(stdscr=stdscr, start=0, end=width, step=1, y=yy)
-        stdscr.move(linesStartingY, PREFIXlen)
-        tmpY = linesStartingY;
-        tmpX = PREFIXlen
+        yTMP = linesStartingY
+        xTMP = PREFIXlen
+        scrollWhenYOverflow(yTMP, xTMP)
+        stdscr.move(yTMP, xTMP)
+        lastChar=False
         for character in code:
-            stdscr.addstr(character)
-            tmpX += 1
-            if tmpX >= width: tmpX = 0; tmpY += 1
-            stdscr.move(tmpY, tmpX)
+            handleSyntaxHighlighting(ord(character),False,ord(lastChar) if lastChar else False,(yTMP,xTMP))
+            xTMP += 1
+            if xTMP >= width or character == '\n':
+                xTMP = 0; yTMP += 1
+                if inIndent and not inString and character == '\n':
+                    stdscr.addstr(INDENT, curses.color_pair(2))
+                    xTMP = INDENTlen
+            stdscr.move(yTMP, xTMP)
+            lastChar = character
 
-    def bigClear():
-        for _y in range(height - 1, linesStartingY, -1):
-            for _ in range(height - linesStartingY + 3):
+    def bigClear(customStartingY=False):
+        if customStartingY:
+            startingY=customStartingY
+        else:
+            startingY=linesStartingY
+        for _y in range(height - 1, startingY, -1):
+            for _ in range(height - startingY + 3):
                 delete_line(stdscr=stdscr, start=0, end=width, step=1, y=_y)
-        for _ in range(y - linesStartingY + 2):
-            delete_line(stdscr=stdscr, start=PREFIXlen, end=width, step=1, y=linesStartingY)
-        stdscr.move(linesStartingY, PREFIXlen)
+        for _ in range(y - startingY + 2):
+            delete_line(stdscr=stdscr, start=PREFIXlen, end=width, step=1, y=startingY)
+        stdscr.move(startingY, PREFIXlen)
+
+    def addCharacter(character,color=False,customYX=(False,False)):
+        # expects one character in chr()
+        stdscr.delch()
+        tmpY=customYX[0] if customYX[1] else y
+        tmpX=customYX[1] if customYX[1] else x
+        if x-1 < 0: tmpY-=1 ; tmpX=width
+        tmpX-=1
+        stdscr.move(tmpY, tmpX)
+        if color:
+            stdscr.addstr(character, color)
+        else:
+            stdscr.addstr(character)
+    def handleSyntaxHighlighting(c,deleteLastChar=True,lastChar=False,customYX=(False,False)):
+        nonlocal inString
+        # expects one character, in ord()
+        tmpY=customYX[0] if customYX[1] else y
+        tmpX=customYX[1] if customYX[1] else x
+        GREEN = curses.color_pair(5)
+        MAGENTA = curses.color_pair(6)
+        charC=chr(c)
+        if lastChar: lastCharC=chr(lastChar)
+        if c in {34,39}: # string quotes
+            if lastChar and lastCharC in 'frb':
+                stdscr.move(tmpY, tmpX-2)
+                stdscr.addstr(chr(lastChar), GREEN)
+            if not inString:
+                inString = c
+            elif c == inString:
+                inString = False
+            if deleteLastChar:
+                addCharacter(charC, GREEN, customYX)
+            else:
+                stdscr.addstr(charC, GREEN)
+        elif inString:
+            if deleteLastChar:
+                addCharacter(charC, GREEN, customYX)
+            else:
+                stdscr.addstr(charC, GREEN)
+        elif charC in "0123456789" and ((lastChar and lastCharC in "0123456789 .()*+-/,=&^%$#@{}") or not lastChar):
+            if lastChar and lastCharC == '.':
+                stdscr.move(tmpY, tmpX-2)
+                stdscr.addstr('.', MAGENTA)
+            if deleteLastChar:
+                addCharacter(charC, MAGENTA, customYX)
+            else:
+                stdscr.addstr(charC, MAGENTA)
+        elif not deleteLastChar:
+            stdscr.addstr(charC)
+
+    def syntaxHighlightString(theString, deleteLastChar=True, lastChar=False, customYX=(False, False)):
+        # a loop wrapper for handleSyntaxHighlighting
+        tmpLastChar = lastChar if lastChar else False
+        for char in theString:
+            handleSyntaxHighlighting(ord(char), deleteLastChar=deleteLastChar, lastChar=tmpLastChar, customYX=customYX)
+            tmpLastChar = ord(char)
+
+    def scrollWhenYOverflow(customY=False, customX=False):
+        theY = customY if customY else y+1
+        theX = customX if customX else 0
+        if theY >= height:
+            stdscr.scroll(1)
+        else:
+            stdscr.move(theY, theX)
 
     linesStartingY = prefaceLEN
     history_idx = 0
-    while True:
+    while True: # begin the REPL
         if childPoll() == 0: exitRoutine()
-        c = stdscr_getch()
+        if debug and debugTryCommands:
+            c = debugTryCommands.pop(0)
+            if c not in {-1, 410, 337, 534, 526, 575, 567}:
+                stdscr.addstr(chr(c))
+        else:
+            c = stdscr_getch()
         codeLength = len(code)
         # notetoself: x and y are cursor position
         y, x = stdscr_getyx()
@@ -382,21 +468,35 @@ def main(stdscr):
             debugFileOut = True
             if bash_history:
                 # earlier history
-                if history_idx == len(bash_history):
+                if history_idx <= len(bash_history):
                     history_idx -= 1
-                    if history_idx < 0: history_idx = 0
-                    code = bash_history[-1]
+                if history_idx < 0: history_idx=0
+                tmpNLSpace = '\n'+SPACE
+                if inIndent:
+                    if code.count(tmpNLSpace) > 0 and code.split(tmpNLSpace)[-1]:
+                        code = ''.join(['\n'+SPACE+_ for _ in code.split(tmpNLSpace)[:-1]])[len(tmpNLSpace):]+'\n'+(SPACE*currentIndent)
+                    code += bash_history[history_idx]
                 else:
-                    history_idx -= 1
-                    if history_idx < 0: history_idx=0
-                    code = bash_history[history_idx]
+                    code  = bash_history[history_idx]
+                    if y == linesStartingY and checkIfIndent(code) == None: inIndent = False
 
                 if y > linesStartingY: # when an entry is multiline, clear the heck out of everything
                     bigClear()
                 else:
                     delete_line(stdscr=stdscr, start=x, end=PREFIXlen - 1, step=-1, y=y)
-                stdscr.addstr(bash_history[history_idx])
-                codePosition += len(bash_history[history_idx])
+                if code.count(tmpNLSpace) > 0:
+                    tmpFirstLine=True
+                    #linesStartingY=y
+                    for nl in code.split(tmpNLSpace):
+                        if not tmpFirstLine:
+                            stdscr.addstr('\n'+INDENT+SPACE, curses.color_pair(2))
+                        syntaxHighlightString(nl,False)
+                        if tmpFirstLine: tmpFirstLine = False; inIndent = True
+                    codePosition += len(bash_history[history_idx].replace('\n',''))
+
+                else:
+                    syntaxHighlightString(bash_history[history_idx],False)
+                    codePosition += len(bash_history[history_idx])
 
         elif c == curses.KEY_DOWN:
             debugFileOut = True
@@ -404,17 +504,22 @@ def main(stdscr):
                 # later history
                 history_idx += 1
                 if y > linesStartingY: # when an entry is multiline, clear the heck out of everything
-                    bigClear()
+                    bigClear(y)
                 else:
-                    #delete_line(stdscr=stdscr, start=x, end=PREFIXlen - 1, step=1, y=y)
                     delete_line(stdscr=stdscr, start=PREFIXlen, end=width - 1, step=1, y=linesStartingY)
                     stdscr.move(linesStartingY, prefaceLEN+1)
-
-                stdscr.addstr(bash_history[history_idx])
-                code = bash_history[history_idx]
+                syntaxHighlightString(bash_history[history_idx],False)
+                if inIndent:
+                    tmpNLSpace = '\n' + SPACE
+                    if code.count(tmpNLSpace) > 0 and code.split(tmpNLSpace)[-1]:
+                        code = ''.join(['\n'+SPACE+_ for _ in code.split(tmpNLSpace)[:-1]])[len(tmpNLSpace):]+'\n'+(SPACE*currentIndent)
+                    code += bash_history[history_idx]
+                else:
+                    code = bash_history[history_idx]
                 codePosition += len(bash_history[history_idx])
             else:
                 # the most down should be blank
+                code = '' ; codePosition=0
                 history_idx = len(bash_history)
                 bigClear()
                 delete_line(stdscr=stdscr, start=width-1, end=PREFIXlen-1, step=-1, y=linesStartingY)
@@ -483,7 +588,7 @@ def main(stdscr):
                 if c == 330: stdscr.move(y, x - 1) # DEL key
                 hinted = display_hint(stdscr, y, x, code, lastCursorX=0, after_appending=0, hinted=hinted, maxX=0)
 
-                if inIndent and exitIndent(code) != None and len(deIndent(code)[-2]) > len(code.split('\n')[-1]):
+                if inIndent and exitIndent(code) != None and len(tuple(deIndent(code))) > 1 and len(deIndent(code)[-2]) > len(code.split('\n')[-1]):
                     # take deindent to next line
                     currentIndent = len(code.split('\n')[-1])//SPACElen
             else:
@@ -502,16 +607,22 @@ def main(stdscr):
             elif code and ((inIndent and exitIndent(code) == None) or (not inIndent and checkIfIndent(code) != None)):
                 if checkIfIndent(code.split('\n'+(SPACE*currentIndent))[-1]) != None:
                     currentIndent+=1
+                bash_history.append(code.split('\n')[-1])
+                # ^ entering the current line into the bash history instead of the whole code ensures stability when going up and down history
                 code+='\n'+(SPACE*currentIndent) ; codePosition+=1+(SPACElen*currentIndent)
-                stdscr.move(y+1, 0)
+                if y+1 < height:
+                    stdscr.move(y+1, 0)
+                else:
+                    stdscr.scroll(1)
                 stdscr.addstr(INDENT+(SPACE*currentIndent), curses.color_pair(2))
                 lastCursorX = INDENTlen
                 inIndent = True
             else:
-                inIndent = False ;  currentIndent=0
+                if inIndent and code and code[-1] == '\n': code=code[:-1]
                 if code:
                     # evaluate the line/block
-                    if code and (not bash_history or code != bash_history[-1]):
+                    if inIndent and exitIndent(code) != None and code[-1]=='\n': code+=SPACE
+                    if code and (not bash_history or code != bash_history[-1]) and not inIndent:
                         bash_history.append(code)
                     history_idx = len(bash_history)
                     delete_line(stdscr=stdscr, start=width - 1, end=PREFIXlen + len(code) - 1, step=-1, y=y)
@@ -531,11 +642,11 @@ def main(stdscr):
                         if isWindows:
                             stdscr.move(y, 0)
                         else:
-                            stdscr.move(y + 1, 0)
+                            scrollWhenYOverflow(y+1,0)
                     elif isWindows:
                         stdscr.move(y - 1, width - 1)
 
-                    tmpCodeYLen = (PREFIXlen + len(code)) // width + 1
+                    tmpCodeYLen = (PREFIXlen + len(code)) // width
                     if not isWindows and not firstLine and tmpCodeYLen >= 1 and y+1 < height:
                         tmpIndex=(codePosition//width)+1
                         if tmpIndex < tmpCodeYLen:
@@ -618,8 +729,11 @@ def main(stdscr):
                     if isWindows:
                         stdscr.move(y, 0)
                     else:
-                        try: stdscr.move(y + 1, 0)
-                        except: stdscr.addstr('\n')
+                        scrollWhenYOverflow(y+1,0)
+
+                inIndent = False
+                currentIndent = 0
+                inString=False
                 y, x = stdscr.getyx()
                 linesStartingY = y
                 stdscr.addstr(PREFIX, curses.color_pair(2))
@@ -650,42 +764,55 @@ def main(stdscr):
             skipToCharacter(stdscr, x, y)
         elif c in CTRL_RIGHT:
             skipToCharacter(stdscr, x, y, direction='right')
-        elif c == 575: # ctrl_up
+        elif c in {575, 567}: # ctrl_up
             # don't combine with elif, we want to trap that input
             if y > linesStartingY:
                 if y-1 == linesStartingY and x <= PREFIXlen:
                     stdscr.move(y - 1, PREFIXlen)
                     codePosition-=width-PREFIXlen
+                elif inIndent:
+                    stdscr.move(y - 1, x)
+                    tmp = len(code.split('\n')[y-linesStartingY])
+                    codePosition=len(''.join(code.split('\n'))[:tmp])
                 else:
                     stdscr.move(y-1, x)
                     codePosition-=width
-        elif c == 534: # ctrl_down
-            if codeLength > width:
+        elif c in {534, 526}: # ctrl_down
+            if inIndent and (y+1-linesStartingY) <= code.count('\n'):
+                stdscr.move(y + 1, x)
+                codePosition+=len(code[codePosition:])
+                if codePosition+INDENTlen > codeLength:
+                    code+=' '*(codePosition+INDENTlen-codeLength)
+                    codePosition+=(codePosition+INDENTlen-codeLength)
+
+            elif codeLength > width:
                 if codeLength // width > codePosition // width and abs((y-linesStartingY) * width + x) <= codeLength:
                     stdscr.move(y + 1, x)
                     codePosition = abs((y+1-linesStartingY) * width + x)
                     if y == linesStartingY: codePosition-=PREFIXlen
-        elif c in {-1, 410}: pass
+        elif c in {-1, 410, 337}: pass
         else:
             debugFileOut = True
             if codePosition == codeLength:
+                handleSyntaxHighlighting(c,lastChar=ord(code[-1]) if code else False)
                 code += chr(c)
                 codePosition += 1
                 lastCursorX = x
-                if code[-1] != ' ':
-                    hinted = display_hint(stdscr, y, x, code.split()[-1], lastCursorX, after_appending, hinted, width)
-                else:
-                    # if a space is done on a suggestion, clear the suggestion
-                    clear_suggestion(stdscr=stdscr, start=lastCursorX, end=width, step=1, y=y)
+                if not inString:
+                    if code[-1] != ' ':
+                        hinted = display_hint(stdscr, y, x, code.split()[-1], lastCursorX, after_appending, hinted, width)
+                    else:
+                        # if a space is done on a suggestion, clear the suggestion
+                        clear_suggestion(stdscr=stdscr, start=lastCursorX, end=width, step=1, y=y)
             else:
                 if codePosition == 0:
                     code = chr(c) + code[codePosition:]
                 else:
                     code = code[:codePosition] + chr(c) + code[codePosition:]
                 codePosition += 1
-                if len(code)+PREFIXlen < width: # one-line, update in-place
+                if len(code)+PREFIXlen < width and not inIndent: # one-line, update in-place
                     delete_line(stdscr=stdscr, start=x - 1 + len(code[codePosition:]), end=x - 1, step=-1, y=y)
-                    stdscr.addstr(code[codePosition:])
+                    syntaxHighlightString(code[codePosition:])
                 else: # multi-liner, redraw entire code
                     redraw(stdscr)
 
